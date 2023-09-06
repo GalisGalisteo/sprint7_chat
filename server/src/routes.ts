78e2b8337dbc2a908e05f74eb3b852f096676716
Galis <galis.travelling@gmail.com>
2023-09-06 21:46:44 +0200

    adding authentification

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { createMessage, createUser, handleLogin } from "./application/controller";
+import authenticate from "./infrastructure/middleware/auth";
 
 const router = express.Router();
 
 router.post("/login", handleLogin)
 router.post("/user", createUser);
-router.post("/message/:user_id", createMessage)
+router.post("/message/:user_id", authenticate, createMessage)
 
 export default router;
